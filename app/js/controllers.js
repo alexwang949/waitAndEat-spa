a0b933797734d173a47a7b40acd67944a270be44
Alex Wang <alexwang949@gmail.com>
2015-09-03 16:24:42 -0400

    added model for form, added ng-submit, defined function for form submission

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,5 +7,12 @@ myApp.controller('landingPageController', [function() {
 
 }]);
 myApp.controller('waitListController', ['$scope', function($scope) {
-	$scope.test = "hello";
+	$scope.parties = [];
+	$scope.party = {name: '', phone: '', size: ''};
+
+	// define function to submit form
+	$scope.saveParty = function() {
+		$scope.parties.push($scope.party);
+		$scope.party = {name: '', phone: '', size: ''};
+	};
 }]);
